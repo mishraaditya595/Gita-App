1a2c23610c5dbff1c59f77ecb624785de4b8e8bb
mishraaditya595 <mishraaditya595@gmail.com>
2022-12-14 15:24:16 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/sbg/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.sbg
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:sbg/ui/about_page.dart';
+import 'package:sbg/ui/home_page.dart';
+import 'package:sbg/ui/saved_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      selectedIndex = index;
+    });
+  }
+
+  List<Widget> pages = [
+    const HomePage(),
+    const SavedPage(),
+    const AboutPage()
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      body: pages[selectedIndex] ,
+      bottomNavigationBar: BottomNavigationBar(
+        elevation: 10,
+        currentIndex: selectedIndex,
+        selectedLabelStyle: const TextStyle(fontWeight: FontWeight.bold),
+        selectedIconTheme: IconThemeData(color: Colors.orange[700], size: 30),
+        onTap: _onItemTapped,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.save), label: "Saved"),
+          BottomNavigationBarItem(icon: Icon(Icons.question_mark), label: "About")
+        ],
+      ),
+    );
+  }
+
+}
