4b4a469b42559c441f6ef9f7056cf24b3b91ac20
Aditya Mishra <aditya.mishra@salescode.ai>
2023-06-26 13:36:42 +0530

    loading_screen.dart ui changes

--- lib/ui/loading/screen/loading_screen.dart
+++ lib/ui/loading/screen/loading_screen.dart
@@ -4,6 +4,7 @@ import 'package:get_it/get_it.dart';
 import 'package:loading_progress_indicator/loading_progress_indicator.dart';
 import 'package:loading_progress_indicator/progress_indicator/ball_spin_fade_loader_progress_indicator.dart';
 import 'package:sbg/ui/loading/service/loading_service.dart';
+import 'package:sbg/utils/hexcolor.dart';
 
 import '../../bottombar/screen/bottom_bar.dart';
 
@@ -29,22 +30,33 @@ class _LoadingScreenState extends State<LoadingScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: HexColor("A2D9C7"),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            LoadingProgressIndicator(
-              indicator: BallSpinFadeLoaderProgressIndicator(),
-              size: 70,
-              color: Colors.orangeAccent,
+            Container(
+              width: 150,
+              height: 150,
+              padding: const EdgeInsets.all(8.0),
+              child: Image.asset("assets/images/bhagavad-gita.png"),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: LoadingProgressIndicator(
+                indicator: BallSpinFadeLoaderProgressIndicator(),
+                size: 70,
+                color: Colors.white,
+              ),
             ),
             Container(
               width: MediaQuery.of(context).size.width * 0.9,
               padding: EdgeInsets.all(15.0),
               child: const Text(
-                  "Please wait while we fetch the divine knowledge for you.\n\nहरि ओम तत् सत् |",
+                  "Loading...",
                 textAlign: TextAlign.center,
                 style: TextStyle(
+                  color: Colors.white,
                   fontSize: 15,
                   fontWeight: FontWeight.bold,
                 ),
