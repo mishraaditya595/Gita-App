bc9ae354d3714baaa4856a2d2cb12a10c38e9ec3
Aditya Mishra <aditya.mishra@salescode.ai>
2023-07-20 11:39:09 +0530

    database_service.dart implemented as per hive

--- lib/services/db/database_service.dart
+++ lib/services/db/database_service.dart
@@ -1,6 +1,12 @@
+import 'package:flutter/foundation.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:injectable/injectable.dart';
+import 'package:sbg/models/chapter_detailed_model.dart';
+import 'package:sbg/models/chapter_summary_model.dart';
+import 'package:sbg/models/data_sync_model.dart';
+import 'package:sbg/models/last_read_model.dart';
+import 'package:sbg/models/verse_bookmark_model.dart';
 
 
 @Singleton()
@@ -17,6 +23,27 @@ class DatabaseService<T> {
 
     } else {
       await Hive.initFlutter();
+      try {
+        Hive.registerAdapter(ChapterDetailedModelAdapter());
+        Hive.registerAdapter(ChapterSummaryModelAdapter());
+        Hive.registerAdapter(DataSyncModelAdapter());
+        Hive.registerAdapter(LastReadModelAdapter());
+        Hive.registerAdapter(VerseBookmarkModelAdapter());
+      } on HiveError {}
+
+      await Hive.openBox<ChapterDetailedModel>(describeEnum(DbModel.ChapterDetailedModel));
+      await Hive.openBox<ChapterSummaryModel>(describeEnum(DbModel.ChapterSummaryModel));
+      await Hive.openBox<DataSyncModel>(describeEnum(DbModel.DataSyncModel));
+      await Hive.openBox<LastReadModel>(describeEnum(DbModel.LastReadModel));
+      await Hive.openBox<VerseBookmarkModel>(describeEnum(DbModel.VerseBookmarkModel));
     }
   }
+}
+
+enum DbModel {
+  ChapterDetailedModel,
+  ChapterSummaryModel,
+  DataSyncModel,
+  LastReadModel,
+  VerseBookmarkModel
 }
