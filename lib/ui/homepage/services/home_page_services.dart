5c1ffa264dff9b2c58bc8c697717bb56239ff73c
Aditya Mishra <aditya.mishra@salescode.ai>
2023-07-20 12:08:50 +0530

    home_page_services.dart refactored

--- lib/ui/homepage/services/home_page_services.dart
+++ lib/ui/homepage/services/home_page_services.dart
@@ -1,10 +1,11 @@
+import 'package:flutter/foundation.dart';
 import 'package:get_it/get_it.dart';
+import 'package:hive/hive.dart';
 import 'package:injectable/injectable.dart';
 
 import '../../../models/chapter_detailed_model.dart';
 import '../../../models/chapter_summary_model.dart';
 import '../../../models/last_read_model.dart';
-import '../../../objectbox.g.dart';
 import '../../../services/db/database_service.dart';
 
 @Singleton()
@@ -15,38 +16,35 @@ class HomePageServices {
 
   //<--- get all chapters summary --->
   List<ChapterSummaryModel> getAllChapters() {
-    Store store = databaseService.getStore()!;
     Box<ChapterSummaryModel> chapterSummaryModelBox =
-        store.box<ChapterSummaryModel>();
-    QueryBuilder<ChapterSummaryModel> queryBuilder = chapterSummaryModelBox
-        .query()
-      ..order(ChapterSummaryModel_.chapterNumberInt);
-    Query<ChapterSummaryModel> query = queryBuilder.build();
-    List<ChapterSummaryModel> chapterSummaryList = query.find();
-    return chapterSummaryList;
+    databaseService.getStore<ChapterSummaryModel>(describeEnum(DbModel.ChapterSummaryModel));
+
+    List<ChapterSummaryModel> list = chapterSummaryModelBox.values.toList();
+
+    list.sort((a, b) => a.chapterNumberInt.compareTo(b.chapterNumberInt));
+    
+    return list;
   }
 
   //<--- get last read verse --->
   List<LastReadModel> getLastReadVerse() {
-    Store store = databaseService.getStore()!;
-    Box<ChapterDetailedModel> chapterDetailedModelBox =
-        store.box<ChapterDetailedModel>();
-    Box<LastReadModel> lastReadModelBox = store.box<LastReadModel>();
-    List<LastReadModel> lastReadList = lastReadModelBox.getAll();
+    
+    Box<LastReadModel> lastReadModelBox =
+        databaseService.getStore<LastReadModel>(describeEnum(DbModel.LastReadModel));
+    
+    List<LastReadModel> lastReadList = lastReadModelBox.values.toList();
     return lastReadList;
   }
 
   List<ChapterDetailedModel> getChapterDetailedList(
       int chapterNumber, int verseNumber) {
-    Store store = databaseService.getStore()!;
     Box<ChapterDetailedModel> chapterDetailedModelBox =
-        store.box<ChapterDetailedModel>();
-    QueryBuilder<ChapterDetailedModel> queryBuilder = chapterDetailedModelBox
-        .query(ChapterDetailedModel_.chapterNumber.equals("$chapterNumber") &
-            ChapterDetailedModel_.verseNumber.equals("$verseNumber"))
-      ..order(ChapterDetailedModel_.verseNumberInt);
-    Query<ChapterDetailedModel> query = queryBuilder.build();
-    List<ChapterDetailedModel>? queryList = query.find();
+        databaseService.getStore<ChapterDetailedModel>(describeEnum(DbModel.ChapterDetailedModel));
+
+    List<ChapterDetailedModel>? queryList = chapterDetailedModelBox.values.where(
+            (element) =>
+                element.verseNumber == "$verseNumber" &&
+                element.chapterNumber == "$chapterNumber").toList();
 
     return queryList;
   }
